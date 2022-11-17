e83ce6b71100cc0a07d58e7f71880f940262da91
kanake10 <10kanake@gmail.com>
2022-11-17 23:37:40 +0300

    shift the constants to TestUtils

--- app/src/test/java/com/eeyan/cryptapp/data/mappers/DtoToDomainKtTest.kt
+++ app/src/test/java/com/eeyan/cryptapp/data/mappers/DtoToDomainKtTest.kt
@@ -1,13 +1,7 @@
 package com.eeyan.cryptapp.data.mappers
 
-import com.eeyan.cryptapp.data.remote.dto.CoinDetailDto
-import com.eeyan.cryptapp.data.remote.dto.CoinDto
-import com.eeyan.cryptapp.data.remote.dto.Whitepaper
-import com.eeyan.cryptapp.data.utils.LinksExtendedUtil
-import com.eeyan.cryptapp.data.utils.TeamMemberUtil
-import com.eeyan.cryptapp.data.utils.links
-import com.eeyan.cryptapp.domain.model.Coin
-import com.eeyan.cryptapp.domain.model.CoinDetail
+
+import com.eeyan.cryptapp.data.utils.*
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
@@ -17,62 +11,15 @@ class DtoToDomainKtTest {
 
     @Test
     fun `confirm that CoinDto toCoin results to Coin`() {
-        val coinDto = CoinDto(
-            id = "android",
-            is_active = false,
-            is_new = true,
-            name = "name",
-            rank = 10,
-            symbol = "symbol",
-            type = "type"
-        )
-        val coin = Coin(
-            id = "android",
-            is_active = false,
-            name = "name",
-            rank = 10,
-            symbol = "symbol",
-        )
+        val coinDto = coinDtoTestUtil
+        val coin = coinTestUtil
         assertThat(coinDto.toCoin()).isEqualTo(coin)
     }
 
     @Test
     fun `confirm that toCoinDetail toDomain results to Coin`() {
-        val coinDetailDto = CoinDetailDto(
-            coinId = "bitcoin",
-            isActive = false,
-            is_new = true,
-            name = "name",
-            description = "Bitcoin is a decentralized digital currency",
-            rank = 10,
-            symbol = "symbol",
-            tags = listOf(),
-            team = listOf(TeamMemberUtil),
-            development_status = "",
-            first_data_at = "",
-            hardware_wallet = false,
-            hash_algorithm = "",
-            last_data_at = "",
-            links = links,
-            links_extended = listOf(LinksExtendedUtil),
-            message = "",
-            open_source = true,
-            org_structure = "",
-            proof_type = "",
-            started_at = "",
-            whitepaper = Whitepaper(link = "", thumbnail = ""),
-            type = ""
-        )
-        val coinDetail = CoinDetail(
-            coinId = "bitcoin",
-            isActive = false,
-            name = "name",
-            description = "Bitcoin is a decentralized digital currency",
-            rank = 10,
-            symbol = "symbol",
-            tags = listOf(),
-            team = listOf(TeamMemberUtil)
-        )
+        val coinDetailDto = coinDetailDtoTestUtil
+        val coinDetail = coinDetailTestUtil
         assertThat(coinDetailDto.toCoinDetail()).isEqualTo(coinDetail)
     }
 }
