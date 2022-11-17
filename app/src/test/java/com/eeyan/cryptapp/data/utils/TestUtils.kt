d5e5b259f0b5f6eff08c705c4ddd9dcc86786092
kanake10 <10kanake@gmail.com>
2022-11-17 23:37:30 +0300

    write more testing utils

--- app/src/test/java/com/eeyan/cryptapp/data/utils/TestUtils.kt
+++ app/src/test/java/com/eeyan/cryptapp/data/utils/TestUtils.kt
@@ -1,9 +1,8 @@
 package com.eeyan.cryptapp.data.utils
 
-import com.eeyan.cryptapp.data.remote.dto.Links
-import com.eeyan.cryptapp.data.remote.dto.LinksExtended
-import com.eeyan.cryptapp.data.remote.dto.Stats
-import com.eeyan.cryptapp.data.remote.dto.TeamMember
+import com.eeyan.cryptapp.data.remote.dto.*
+import com.eeyan.cryptapp.domain.model.Coin
+import com.eeyan.cryptapp.domain.model.CoinDetail
 
 val TeamMemberUtil = TeamMember(
     id = "xyz",
@@ -24,4 +23,59 @@ val LinksExtendedUtil = LinksExtended(
     stats = Stats(contributors = 1, followers = 2, stars = 3, subscribers = 4),
     type = "",
     url = ""
+)
+
+val coinDtoTestUtil = CoinDto(
+    id = "android",
+    is_active = false,
+    is_new = true,
+    name = "name",
+    rank = 10,
+    symbol = "symbol",
+    type = "type"
+)
+
+val coinTestUtil = Coin(
+    id = "android",
+    is_active = false,
+    name = "name",
+    rank = 10,
+    symbol = "symbol",
+)
+
+val coinDetailDtoTestUtil = CoinDetailDto(
+    coinId = "bitcoin",
+    isActive = false,
+    is_new = true,
+    name = "name",
+    description = "Bitcoin is a decentralized digital currency",
+    rank = 10,
+    symbol = "symbol",
+    tags = listOf(),
+    team = listOf(TeamMemberUtil),
+    development_status = "",
+    first_data_at = "",
+    hardware_wallet = false,
+    hash_algorithm = "",
+    last_data_at = "",
+    links = links,
+    links_extended = listOf(LinksExtendedUtil),
+    message = "",
+    open_source = true,
+    org_structure = "",
+    proof_type = "",
+    started_at = "",
+    whitepaper = Whitepaper(link = "", thumbnail = ""),
+    type = ""
+)
+
+val coinDetailTestUtil = CoinDetail(
+    coinId = "bitcoin",
+    isActive = false,
+    name = "name",
+    description = "Bitcoin is a decentralized digital currency",
+    rank = 10,
+    symbol = "symbol",
+    tags = listOf(),
+    team = listOf(TeamMemberUtil)
 )
